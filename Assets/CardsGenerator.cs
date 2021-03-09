869fc6371dbc99726c549807a2aa17cb29e3878f
Sebastian Pięta <spieta2@gmail.com>
2021-03-09 16:01:40 +0100

    Extend CardsGenerator

--- Assets/CardsGenerator.cs
+++ Assets/CardsGenerator.cs
@@ -6,41 +6,85 @@ public class CardsGenerator : MonoBehaviour
 {
     private class Card
     {
-        private int id_;
+        public Card(int id, GameObject cardObj)
+        {
+            id_ = id;
+            cardObj_ = cardObj;
+        }
 
+        private int id_;
+        public GameObject cardObj_ { get; }
     }
 
     [SerializeField]
-    private readonly string cardsContainerTag_;
+    private string cardsContainerTag_;
+
+    [SerializeField]
+    private float offset_;
+    [SerializeField]
+    private int maxRowLength_;
+
+    enum Difficulty { _2x2 = 4, _2x4 = 8, _4x4 = 16 }
+
+    private Difficulty difficulty_ = Difficulty._4x4;
 
     private Transform cardsContainer_;
     private List<Card> cardsStorage_;
 
+    private List<Card> currentGame_;
+
+    public GameObject cardPrefab_;
+
     private void Awake()
     {
         cardsContainer_ = GameObject.FindGameObjectWithTag(cardsContainerTag_).transform;
-        // if (cardsContainer == null) throw new System.Exception("Couldn't find cards container with tag: " + cardsContainerTag);
         InitializeCardPool();
+        currentGame_ = new List<Card>();
     }
 
     private void InitializeCardPool()
     {
         cardsStorage_ = new List<Card>();
-        // TODO: add cards from ScriptableObjects
+        for (int i = 0; i < (int) difficulty_; i++)
+        {
+            GameObject cardObj = Instantiate(cardPrefab_, cardsContainer_);
+            cardObj.SetActive(false);
+            Card card = new Card(i, cardObj);
+            cardsStorage_.Add(card);
+        }
     }
 
     void Start()
     {
-        Generate();
+        DeployCards();
     }
 
-    void Update()
+    public void DeployCards()
     {
-        
+        currentGame_.AddRange(cardsStorage_);
+        Shuffle();
+        PlaceCards();
     }
 
-    public void Generate()
+    private void Shuffle()
     {
+        int n = currentGame_.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n);
+            Card value = currentGame_[k];
+            currentGame_[k] = currentGame_[n];
+            currentGame_[n] = value;
+        }
+    }
 
+    private void PlaceCards()
+    {
+        for (int i = 0; i < currentGame_.Count; i++)
+        {
+            currentGame_[i].cardObj_.transform.position = new Vector2(i % maxRowLength_ * offset_, (Mathf.FloorToInt(i / maxRowLength_) - 1) * offset_);
+            currentGame_[i].cardObj_.SetActive(true);
+        }
     }
 }
